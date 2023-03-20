ca7e4b7fa216d70182c33ce634dece916f45e062
withpaul <jongjin4749@gmail.com>
2023-03-21 08:37:40 +0900

    H2 셋팅완료

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -2,11 +2,13 @@ package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class JpashopApplicationTests {
 
 	@Test
+	@Transactional
 	void contextLoads() {
 	}
 
